b9f530b176b04dd6e338184ae130dd3913bfb18a
Philipp Meier <philipp.meier@eawag.ch>
2014-09-03 17:32:19 +0200

    Implemented reclassification of ambiguous pixels (didn't work as intended).

--- reclassify.py
+++ reclassify.py
@@ -65,7 +65,7 @@ class RGBlayer(object):
                                     write_band, self.x_size, self.y_size,
                                     gdal.GDT_UInt32)
 
-    def reclassify(self, n):
+    def reclassify(self, n, classify_ambiguous=False):
 
         print "Classify ..."
         # Create unique values from all bands
@@ -79,14 +79,88 @@ class RGBlayer(object):
         frequencies = zip(unique_values, count)
         frequencies.sort(key=lambda tup: tup[1], reverse=True)
         # The first n most frequent classes are the classes we need:
-        pixclasses = frequencies[0:n]
+        pixclasses = frequencies[:n]
         classified = np.zeros(sumband.shape, np.uint8)
+
+        # get colors
+        if classify_ambiguous:
+            red = self.inputdata[0, :, :].flatten()
+            green = self.inputdata[1, :, :].flatten()
+            blue = self.inputdata[2, :, :].flatten()
+            img_class_colors = dict()
+
         for i, pixclass in enumerate(pixclasses):
-            classified[sumband == pixclass[0]] = i
-        # Now it's time to handle ambiguous cases
-        # will be added later
+            mask = sumband == pixclass[0]
+            classified[mask] = i
+            if classify_ambiguous:
+                img_class_colors[i] = dict(r=red[mask][0],
+                                           g=green[mask][0],
+                                           b=blue[mask][0])
+
+        if classify_ambiguous:
+            print 'Classify ambiguous pixels (go get a cup of coffee) ...'
+            # Now it's time to handle ambiguous cases
+            # first convert RGB to HSV:
+            self.progress(0.0)
+            for i in img_class_colors.keys():
+                color = img_class_colors[i]
+                r = float(color['r']) / 255
+                g = float(color['g']) / 255
+                b = float(color['b']) / 255
+                H, S, V = RGBtoHSV(r, g, b)
+                img_class_colors[i].update(H=H, S=S, V=V)
+
+            it = 0
+            for val, count in frequencies[n:]:
+                mask = sumband == val
+                r = red[mask][0]
+                g = green[mask][0]
+                b = blue[mask][0]
+                H, S, V = RGBtoHSV(r, g, b)
+                old_match = 1000
+                match_idx = -1
+                for i in img_class_colors.keys():
+                    match = get_color_proximity((H, S, V),
+                                                (img_class_colors[i]['H'],
+                                                 img_class_colors[i]['S'],
+                                                 img_class_colors[i]['V']))
+                    if match < old_match:
+                        old_match = match
+                        match_idx = i
+                classified[mask] = match_idx
+                self.progress(float(it) / len(frequencies))
+                it += 1
+            self.progress(1.0)
 
         self.outputdata = tuple(classified)
+
+
+def RGBtoHSV(r, g, b):
+    cmax = max([r, g, b])
+    cmin = min([r, g, b])
+    delta = cmax - cmin
+    if delta == 0:
+        S = 0
+        H = 0
+    else:
+        S = delta / cmax
+        if r >= g and r >= b:
+            H = 60 * ((g - b) / delta % 6)
+        elif g >= r and g >= b:
+            H = 60 * ((b - r) / delta + 2)
+        elif b >= r and b >= g:
+            H = 60 * ((r - g) / delta + 4)
+
+    V = cmax
+    return H, S, V
+
+
+def get_color_proximity(color1, color2):
+    diff1 = min(abs(color1[0] - color2[0]), abs(360 - color2[0] + color1[0]))
+    diff2 = abs(color1[1] - color2[1]) * 50
+    diff3 = abs(color1[2] - color2[2]) * 50
+
+    return diff1 + diff2 + diff3
 
 
 def commandline_parser():
@@ -104,6 +178,8 @@ GeoTIFF with unique classes.
                         help='Overwrite existing file.')
     parser.add_argument('-c', '--classes',
                         help='Number of classes.')
+    parser.add_argument('-a', '--reclass-ambiguous', action='store_true',
+                        help='Classify ambiguous pixels (might take a while).')
 
     return parser
 
@@ -116,5 +192,6 @@ if __name__ == '__main__':
     # Throw an error if file exists before heavy lifting is done
     layer.check_outfile(args.output_file, overwrite=args.overwrite)
     layer.load(args.input_file)
-    layer.reclassify(int(args.classes))
+    layer.reclassify(int(args.classes),
+                     classify_ambiguous=args.reclass_ambiguous)
     layer.save(args.output_file, overwrite=args.overwrite)
